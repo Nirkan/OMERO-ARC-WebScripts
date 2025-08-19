7c904f04b11a32ad609f267c6e8d806f13f0bea2
Nirkan <kandpalniraj88@gmail.com>
2025-08-19 16:40:15 +0200

    Change text -Excel to CSV

--- ImageToFile/ImageToCSV.py
+++ ImageToFile/ImageToCSV.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
-Export image metadata and key-value annotations from a Dataset or Image list to an Excel file.
+Export image metadata and key-value annotations from a Dataset or Image list to a CSV file.
 The result is attached as a downloadable OMERO file annotation.
 """
 
@@ -45,8 +45,8 @@ def extract_image_metadata(images):
 
 def run_script():
     client = scripts.client(
-        "Export_Image_Metadata_Excel.py",
-        "Export image metadata and key-value pairs to Excel, with download link.",
+        "ImageToCSV.py",
+        "Export image metadata and key-value pairs to CSV, with download link.",
         scripts.String("Data_Type", optional=False, values=["Dataset", "Image"],
                        description="Select type of input: Dataset or Image"),
         scripts.List("IDs", optional=False, description="List of Dataset or Image IDs").ofType(scripts.rlong(0))
@@ -78,7 +78,7 @@ def run_script():
         data = extract_image_metadata(images)
         df = pd.DataFrame(data)
 
-        # Save Excel file
+        # Save CSV file
         temp_dir = tempfile.mkdtemp()
 
         if data_type == "Dataset":
